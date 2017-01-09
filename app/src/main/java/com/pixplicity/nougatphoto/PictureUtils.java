4cb1442cc82f9388d4b977da86a89466d7d18a1e
Paul Lammertsma <pflammertsma@gmail.com>
2017-01-09 14:42:26 +0100

    fix orientation of photo when decoding the bitmap

--- app/src/main/java/com/pixplicity/nougatphoto/PictureUtils.java
+++ app/src/main/java/com/pixplicity/nougatphoto/PictureUtils.java
@@ -3,9 +3,45 @@ package com.pixplicity.nougatphoto;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.graphics.Point;
+import android.media.ExifInterface;
+import android.util.Log;
+
+import java.io.IOException;
 
 public class PictureUtils {
+
+    private static final String TAG = PictureUtils.class.getSimpleName();
+
+    private static Bitmap getRotatedBitmap(String path, BitmapFactory.Options options) {
+        Bitmap bm = BitmapFactory.decodeFile(path, options);
+        Bitmap bitmap = bm;
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 1);
+            Log.v(TAG, "orientation: " + orientation);
+            Matrix m = new Matrix();
+            if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+                m.postRotate(180);
+                m.postScale((float) bm.getWidth(), (float) bm.getHeight());
+                bitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(),
+                        bm.getHeight(), m, true);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+                m.postRotate(90);
+                bitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(),
+                        bm.getHeight(), m, true);
+            } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+                m.postRotate(270);
+                bitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(),
+                        bm.getHeight(), m, true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 
     public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
         BitmapFactory.Options options = new BitmapFactory.Options();
@@ -25,7 +61,7 @@ public class PictureUtils {
         }
         options = new BitmapFactory.Options();
         options.inSampleSize = inSampleSize;
-        return BitmapFactory.decodeFile(path, options);
+        return getRotatedBitmap(path, options);
     }
 
     public static Bitmap getScaledBitmap(String path, Activity activity) {
